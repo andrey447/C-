bb7cceab67fbf3e7cce409d1d6b4cff60f90675b
Andrey <andrey@tut.by>
2019-10-05 15:57:21 +0300

    Coment

---
+++ ConsoleApp2/ConsoleApp2/Brits.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+        public enum BreedEnum
+        {
+            None = 0,
+            Brit = 1,
+            Exotic = 2,
+        }
+}
